14fcdf76360b2e4931c8e73a6747ca4ecdd1ca02
Julianweiber <julian.weiber1313@gmail.com>
2021-09-23 15:05:39 -0300

    modif

--- tiendahbv/src/App.js
+++ tiendahbv/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/NavBar/ItemListContainer/ItemListContainer';
 import ItemList from './components/NavBar/ItemListContainer/ItemList';
-
+import { useState,useEffect } from 'react';
 
 
 function App() {
